5030656179ce5b3f2cc519c643ef336401a87969
CherryCoa <130926018+devCherryy@users.noreply.github.com>
2024-02-16 00:00:33 -0400

    Function-SendEmail

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -3,10 +3,10 @@ import parallax from "./components/parallax.js";
 import activeMenu from "./components/selected_menu.js";
 import resetToHome from "./helpers/reload_page.js";
 import updateCopyrightYear from "./helpers/date_updater.js"
-import sendEmail from "./helpers/send_form.js";
+import sendEmail_English  from "./helpers/send_form_in_english.js";
 redirectOnToggle();
 parallax();
 activeMenu();
 resetToHome()
 updateCopyrightYear()
-sendEmail()
+sendEmail_English()
