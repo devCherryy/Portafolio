2214878995275126d974197b53946feaff2776a4
CherryCoa <130926018+devCherryy@users.noreply.github.com>
2024-02-15 23:29:17 -0400

    CopyRigth-insert-2

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -2,7 +2,9 @@ import redirectOnToggle from "./helpers/spanish-english.js";
 import parallax from "./components/parallax.js";
 import activeMenu from "./components/selected_menu.js";
 import resetToHome from "./helpers/reload_page.js";
+import updateCopyrightYear from "./helpers/date_updater.js"
 redirectOnToggle();
 parallax();
 activeMenu();
 resetToHome()
+updateCopyrightYear()
